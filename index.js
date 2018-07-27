e6e6bdc289f5e0e7c5f992a43b357a4649d9307e
Yuanjianxin <1814888800@qq.com>
2018-07-27 17:31:59 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yuanjianxin on 2018/7/23.
+ */
+
+const GlobalFunction=require('./lib/GlobalFunction');
+const MWebCore=require("./lib/MWebCore");
+const WxHttpUtil=require("./lib/WxHttpUtil");
+
+module.exports={
+    GlobalFunction,
+    MWebCore,
+    WxHttpUtil
+};
